7d81c9ae669b3f0dd6ee5c5951fbc88b37670282
dat15021999 <nguyenthanhdat150299@gmail.com>
2019-03-12 00:52:52 +0700

    sua layout

--- Nhom-01/source/app/src/main/java/com/example/jinny/vocabulary/screen/study/CardFragment.java
+++ Nhom-01/source/app/src/main/java/com/example/jinny/vocabulary/screen/study/CardFragment.java
@@ -1,67 +1,67 @@
-package com.example.jinny.vocabulary.screen.study;
-
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
-
-import com.example.jinny.vocabulary.R;
-import com.example.jinny.vocabulary.database.DatabaseManager;
-import com.example.jinny.vocabulary.model.Topic;
-import com.example.jinny.vocabulary.model.Word;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-
-public class CardFragment extends Fragment {
-    private TextView wordTextView;
-    private TextView pronunTextView;
-    private Button button;
-    private Word word;
-    private Fragment cardBackFragment;
-    private Fragment cardFrontFragment;
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Intent intent = getActivity().getIntent();
-        Topic topic = (Topic) intent.getSerializableExtra("topic");
-        word = (Word) getArguments().getSerializable("word");
-        Bundle bundle = new Bundle();
-        bundle.putSerializable("word",word);
-        cardBackFragment = new CardBackFragment();
-        cardBackFragment.setArguments(bundle);
-        cardFrontFragment =  getFragmentManager().findFragmentById(R.id.fragment);
-    }
-
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.card_front_fragment,container,false);
-        wordTextView = v.findViewById(R.id.wordTextView);
-        pronunTextView = v.findViewById(R.id.pronunTextView);
-        button = v.findViewById(R.id.showDetailButton);
-        wordTextView.setText(word.getOrigin());
-        pronunTextView.setText(word.getPronounciation());
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentManager fragmentManager = getActivity().getFragmentManager();
-                FragmentTransaction transaction = fragmentManager.beginTransaction();
-
-                transaction.replace(R.id.fragment,cardBackFragment);
-                transaction.commit();
-            }
-        });
-
-        return  v;
-
-    }
-}
+//package com.example.jinny.vocabulary.screen.study;
+//
+//import android.app.Fragment;
+//import android.app.FragmentManager;
+//import android.app.FragmentTransaction;
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.Button;
+//import android.widget.TextView;
+//
+//import com.example.jinny.vocabulary.R;
+//import com.example.jinny.vocabulary.database.DatabaseManager;
+//import com.example.jinny.vocabulary.model.Topic;
+//import com.example.jinny.vocabulary.model.Word;
+//
+//import androidx.annotation.NonNull;
+//import androidx.annotation.Nullable;
+//
+//public class CardFragment extends Fragment {
+//    private TextView wordTextView;
+//    private TextView pronunTextView;
+//    private Button button;
+//    private Word word;
+//    private Fragment cardBackFragment;
+//    private Fragment cardFrontFragment;
+//
+//    @Override
+//    public void onCreate(@Nullable Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        Intent intent = getActivity().getIntent();
+//        Topic topic = (Topic) intent.getSerializableExtra("topic");
+//        word = (Word) getArguments().getSerializable("word");
+//        Bundle bundle = new Bundle();
+//        bundle.putSerializable("word",word);
+//        cardBackFragment = new CardBackFragment();
+//        cardBackFragment.setArguments(bundle);
+//        cardFrontFragment =  getFragmentManager().findFragmentById(R.id.fragment);
+//    }
+//
+//    @Nullable
+//    @Override
+//    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//        View v = inflater.inflate(R.layout.card_front_fragment,container,false);
+//        wordTextView = v.findViewById(R.id.wordTextView);
+//        pronunTextView = v.findViewById(R.id.pronunTextView);
+//        button = v.findViewById(R.id.showDetailButton);
+//        wordTextView.setText(word.getOrigin());
+//        pronunTextView.setText(word.getPronounciation());
+//
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                FragmentManager fragmentManager = getActivity().getFragmentManager();
+//                FragmentTransaction transaction = fragmentManager.beginTransaction();
+//
+//                transaction.replace(R.id.fragment,cardBackFragment);
+//                transaction.commit();
+//            }
+//        });
+//
+//        return  v;
+//
+//    }
+//}
--- Nhom-01/source/app/src/main/java/com/example/jinny/vocabulary/screen/study/StudyActivity.java
+++ Nhom-01/source/app/src/main/java/com/example/jinny/vocabulary/screen/study/StudyActivity.java
@@ -1,10 +1,5 @@
 package com.example.jinny.vocabulary.screen.study;
 
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
-import android.content.Intent;
-import android.graphics.Color;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -14,22 +9,13 @@ import android.widget.TextView;
 
 import com.example.jinny.vocabulary.R;
 import com.example.jinny.vocabulary.base.BaseActivity;
-import com.example.jinny.vocabulary.database.DatabaseManager;
-import com.example.jinny.vocabulary.model.Topic;
-import com.example.jinny.vocabulary.model.Word;
-
 
 public class StudyActivity extends BaseActivity {
 
-    private Word word;
-    Space topicSpace;
-    TextView origin1, origin2, pronunciation1, pronunciation2, newWord1, newWord2, explaination, type, example, exampleTrans, collapseSpace, extendSpace, topic;
+    TextView origin1, origin2, pronunciation1, pronunciation2, newWord1, newWord2, explaination, type, example, exampleTrans, collapseSpace, extendSpace, topicSpace;
     Button button3, button4, button5;
     ImageView image;
     ImageButton imageButton2;
-    Button nextButton;
-    FragmentManager fragmentManager = getFragmentManager();
-    final FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
     @Override
     protected int getLayoutId() {
         return R.layout.activity_study;
@@ -37,78 +23,23 @@ public class StudyActivity extends BaseActivity {
 
     @Override
     protected void setupUI() {
-        nextButton = findViewById(R.id.next_button);
-        Intent intent = this.getIntent();
-        final Topic topic = (Topic) intent.getSerializableExtra("topic");
-        word = DatabaseManager.getInstance(this).getRandomWord(topic.getId(),0);
-        getWindow().getDecorView().setBackgroundColor(Color.parseColor(topic.getColor()));
-        Bundle bundle = new Bundle();
-        bundle.putSerializable("word",word);
-
-        CardFragment fragment = new CardFragment();
-        fragment.setArguments(bundle);
-
-        final CardBackFragment fragmentBack = new CardBackFragment();
-        fragmentBack.setArguments(bundle);
-
-        fragmentTransaction.add(R.id.fragment, fragment);
-        fragmentTransaction.commit();
-
-        nextButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                word = DatabaseManager.getInstance(getApplication()).getRandomWord(topic.getId(),word.getId());
-                final FragmentTransaction fragmentTransaction1 = fragmentManager.beginTransaction();
-                CardFragment nextCardFragment = new CardFragment();
-                Bundle bundle1 = new Bundle();
-                bundle1.putSerializable("word",word);
-                nextCardFragment.setArguments(bundle1);
-                fragmentTransaction1.replace(R.id.fragment,nextCardFragment);
-                fragmentTransaction1.commit();
-            }
-        });
-
-        /*image = (ImageView) findViewById(R.id.image);
-        Picasso.with(this).load(word.getImageUrl()).into(image);*/
-
-
-        /*FragmentManager fragmentManager = getFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        CardFragment fragment = new CardFragment();
-        fragmentTransaction.add()*/
-
-        /*topicSpace = (Space) findViewById(R.id.topicSpace);
+        topicSpace = (TextView) findViewById(R.id.topicSpace);
         collapseSpace = (TextView) findViewById(R.id.collapseSpace);
         extendSpace = (TextView) findViewById(R.id.extendSpace);
 
-        topic = (TextView) findViewById(R.id.topic);
-        topic.setText(topicName);
-
         origin1 = (TextView) findViewById(R.id.origin1);
         origin2 = (TextView) findViewById(R.id.origin2);
-        origin2.setText(word.getOrigin());
-        origin1.setText(word.getOrigin());
 
         pronunciation1 = (TextView) findViewById(R.id.pronunciation1);
         pronunciation2 = (TextView) findViewById(R.id.pronunciation2);
-        pronunciation1.setText(word.getPronounciation());
-        pronunciation2.setText(word.getPronounciation());
-
 
         newWord1 = (TextView) findViewById(R.id.newWord1);
         newWord2 = (TextView) findViewById(R.id.newWord2);
 
         explaination = (TextView) findViewById(R.id.explaination);
-        explaination.setText(word.getExplaination());
-
         type = (TextView) findViewById(R.id.type);
-        type.setText(word.getType());
-
         example = (TextView) findViewById(R.id.example);
-        example.setText(word.getExample());
-
         exampleTrans = (TextView) findViewById(R.id.exampleTrans);
-        exampleTrans.setText(word.getExample_trans());
 
         button3 = (Button) findViewById(R.id.button3);
         button4 = (Button) findViewById(R.id.button4);
@@ -187,6 +118,6 @@ public class StudyActivity extends BaseActivity {
                 button5.setVisibility(View.GONE);
                 image.setVisibility(View.GONE);
             }
-        });*/
+        });
     }
 }
